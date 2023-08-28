c56f003d2ea649afa247ae0b38978c7de6c00409
Taylor <taylor@bminvestments.co.za>
2023-08-28 14:05:39 +0200

    Initial commit java 17 require jdk/dsk 17 or higher and don't forget to connect to my Sql before run

---
+++ src/main/java/com/WriteHub/org/WriteHub/WriteHubApplication.java
@@ -0,0 +1,13 @@
+package com.WriteHub.org.WriteHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WriteHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WriteHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WriteHub/org/WriteHub/WriteHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WriteHub.org.WriteHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WriteHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
