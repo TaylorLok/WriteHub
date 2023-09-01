176e52e8156cf87199faa6936c19f7d5b0e64111
Jody Heideman <jody@ugoc.co.za>
2023-09-01 16:22:16 +0200

    Added Validation on /create route

--- src/main/java/com/WriteHub/org/WriteHub/Controller/PostController.java
+++ src/main/java/com/WriteHub/org/WriteHub/Controller/PostController.java
@@ -1,25 +1,50 @@
 package com.WriteHub.org.WriteHub.Controller;
 
+import com.WriteHub.org.WriteHub.Models.Posts.PostCategories;
+import com.WriteHub.org.WriteHub.Repository.PostCategoriesRepository;
 import com.WriteHub.org.WriteHub.Repository.PostRepository;
 import com.WriteHub.org.WriteHub.Models.Posts.Posts;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
+@RequestMapping("/post")
 public class PostController {
     @Autowired
     private PostRepository postRepositoryInterface;
+    @Autowired
+    private  PostCategoriesRepository postCategoriesRepository;
 
-    @PostMapping("/add")
-    public String addPost(@RequestPart String title ,@RequestPart String content , @RequestPart String slug , @RequestPart Integer author_id ,  @RequestPart Integer parent_id , @RequestPart Integer post_category_id){
+    @PostMapping("/create/{post_category_id}")
+    public String addPost(@RequestBody Map<String, String> requestPayload , @PathVariable int post_category_id) {
+        String title = requestPayload.get("title");
+        String content = requestPayload.get("content");
+        String slug = requestPayload.get("slug");
+
+        // Check to see if post_category_id exists in database
+       PostCategories postCategoriesById =  postCategoriesRepository.findPostCategoriesById(post_category_id);
+
+        //If not then throw Bad Request Error
+        if (postCategoriesById == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect post category ID");
+        }
+        //Creates a new instance of Posts and populate it with the payload data
         Posts post = new Posts();
         post.setTitle(title);
         post.setContent(content);
         post.setSlug(slug);
-        post.setAuthor_id(author_id);
-        post.setParent_id(parent_id);
+        post.setAuthor_id(1);
+        post.setParent_id(1);
         post.setPost_category_id(post_category_id);
-        return "Added new post";
+        //Saves the set data to the database
+        postRepositoryInterface.save(post);
+        //throws success Response
+        throw new ResponseStatusException(HttpStatus.OK, "Added new post");
+
     }
 
     @GetMapping("/list")
